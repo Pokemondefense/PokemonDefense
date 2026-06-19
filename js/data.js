fd77b82394a343eeb012faa935669a39809c2ed2
POKEMON DEFENSE <doodleseth11@gmail.com>
2026-06-20 03:12:37 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,47 @@
+const POKEMONS = [
+  {
+    name: "Pikachu",
+    hp: 100,
+    attack: 20,
+    range: 120,
+    cost: 50,
+    speed: 1.2
+  },
+  {
+    name: "Charmander",
+    hp: 120,
+    attack: 25,
+    range: 100,
+    cost: 75,
+    speed: 1.0
+  },
+  {
+    name: "Bulbasaur",
+    hp: 150,
+    attack: 15,
+    range: 140,
+    cost: 60,
+    speed: 0.8
+  }
+];
+
+const ENEMIES = [
+  {
+    name: "Rattata",
+    hp: 50,
+    speed: 2,
+    reward: 10
+  },
+  {
+    name: "Zubat",
+    hp: 80,
+    speed: 1.5,
+    reward: 20
+  },
+  {
+    name: "Meowth",
+    hp: 120,
+    speed: 1.2,
+    reward: 30
+  }
+];
